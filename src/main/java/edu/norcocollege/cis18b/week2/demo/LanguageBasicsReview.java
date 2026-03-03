0dbc534c1f7769b6991895c8dbe5d846150c708f
Michael <134744264+FordEx18@users.noreply.github.com>
2026-03-03 02:17:13 +0000

    Created base functions for LanguageBasicsReview

--- src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
+++ src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
@@ -1,21 +1,58 @@
+//Packages
 package edu.norcocollege.cis18b.week2.demo;
-
+//Imports
+import java.util.List;
 /**
  * This class is a review of basic Java language features. It’s not
  * meant to be comprehensive, but it should cover the basics and provide
  * a refresher on syntax and concepts.
- *
- * TODO:
- *  - Demonstrate variable declarations and types
- *  - Demonstrate control flow (if, switch, loops)
- *  - Demonstrate methods and parameters
- *  - Demonstrate basic OOP concepts (classes, objects, inheritance)
  */
+//Begins Here
 public class LanguageBasicsReview {
+    //Variables
+    private String User;
+    private float balance;
+    //Constructor
+    public LanguageBasicsReview(String user, float balance){
+        this.User = user;
+        this.balance = balance;
+    }
+    //Deposit function
+    public void deposit(float amount){
+        balance+=amount;
+        System.out.println("Deposited $" + amount);
+    }
+    //Overload Deposit
+    public void deposit(float amount, String note){
+        balance+=amount;
+        System.out.println("Deposited $" + amount +" | Note: "+ note);
+    }
+    //withdraw Function
+    public void withdraw(float amount)throws InsufficientFundsException{
+        if(amount<balance){
+            throw new InsufficientFundsException("Not enough funds in account!");
+        }else{
+            balance-=amount;
+            System.out.println("withdraw $" + amount);
+        }
+    }
+    //Calculate interest Function
+    public static float calculateInterest(float balance, float rate){
+        return balance*rate;
+    } 
+    //Getter
+    public float getBalance(){
+        return balance;
+    }
+}
 
-    public void demonstrate() {
-        // TODO: Implement demonstration of basic Java features
-        // NOTE: Remove the exception below when you've implemented your code
-        throw new UnsupportedOperationException("Not implemented yet");
+
+
+
+    //Custom Exception Class
+    class InsufficientFundsException extends Exception{
+        public InsufficientFundsException(String message){
+            super(message);
+        }
     }
 }
